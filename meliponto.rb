b4b22690e7afd0d896ec20b883708f8ab5e51e87
Charles Moscofian <charles.moscofian@mercadolivre.com>
2020-11-05 11:52:17 -0300

    release version 0.0.3

--- meliponto.rb
+++ meliponto.rb
@@ -1,9 +1,9 @@
 class Meliponto < Formula
   desc "A simple CLI to facilitate punching hours into the ahgora system"
   homepage "https://github.com/cmoscofian/meliponto"
-  url "https://github.com/cmoscofian/meliponto/archive/v0.0.2.tar.gz"
-  version "0.0.2"
-  sha256 "24b2dc670ba8ddc7d8f5eb3ca8d007cb5ed30636fb4f891a2419335f53b0d91d"
+  url "https://github.com/cmoscofian/meliponto/archive/v0.0.3.tar.gz"
+  version "0.0.3"
+  sha256 "ed5ca9d21a3b640d48594189b8a509da89dbead44a3a8c8d1323865b55052af5"
   license "MIT"
 
   depends_on "go" => :build
